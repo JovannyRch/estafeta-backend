9ee7862151dff345c4269703a7284e300aafdcd9
JovannyRch <jovannyrch@gmail.com>
2023-07-12 12:58:21 -0600

    Update

--- index.js
+++ index.js
@@ -6,25 +6,11 @@ app.use(express.json());
 app.post("/carrier", (req, res) => {
   const carriers = [
     {
-      service_name: "Fedex",
-      description: "Includes tracking and insurance",
-      service_code: "fedex",
-      currency: "MXN",
-      total_price: 100,
-    },
-    {
       service_name: "Estafeta",
       description: "Includes tracking and insurance",
       service_code: "estafeta",
       currency: "MXN",
       total_price: 50.0,
-    },
-    {
-      service_name: "DHL",
-      description: "Includes tracking and insurance",
-      service_code: "dhl",
-      currency: "MXN",
-      total_price: 75.0,
     },
   ];
   res.json(carriers);
